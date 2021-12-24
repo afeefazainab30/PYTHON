6823b45edc7141266ba72c797a939d5688055b6e
Afeefa Zainab <80776182+afeefazainab30@users.noreply.github.com>
2021-12-24 20:34:20 +0530

    Add files via upload
    This is my DAY - 1 Assignments in PYTHON

---
+++ Day - 1 Input Function.py
@@ -0,0 +1,12 @@
+# Write a program that prints the number of characters in a user's name. 
+# You might need to Google for a function that calculates the length of a string.
+
+
+# Example Inputs :
+	# What is your Name ? Shahzaib
+	
+# Example Outputs :
+	# 8
+
+name = input("What is your name?")
+print(len(name))
---
+++ Day - 1 Variables.py
@@ -0,0 +1,24 @@
+ #Eg - 2 Cups - 1 Holds Tea & 1 Holds Coffee. How would you change into one another ??
+
+# 🚨 Don't change the code below 👇
+
+a = input("a: ")
+b = input("b: ")
+
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c 
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+
+print("a: " + a)
+
+print("b: " + b)
